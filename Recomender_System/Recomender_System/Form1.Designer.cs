37826c29fee072ee23fe551c1df75d8e5eeabd08
Idris Karaali <30285751+realka@users.noreply.github.com>
2017-10-30 08:36:36 +0300

    Recomender System ( Pearson Covarience)

---
+++ Recomender_System/Recomender_System/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace Recomender_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.Restoran = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pearson = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.İndex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kosinüs = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Restorant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 32);
+            this.textBox1.MaxLength = 2;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(36, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 32);
+            this.textBox2.MaxLength = 2;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(183, 32);
+            this.textBox3.MaxLength = 2;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(36, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(267, 32);
+            this.textBox4.MaxLength = 2;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(36, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(68, 97);
+            this.textBox5.MaxLength = 2;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(36, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(150, 97);
+            this.textBox6.MaxLength = 2;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(36, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(238, 97);
+            this.textBox7.MaxLength = 2;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(36, 20);
+            this.textBox7.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Ortam Şıklığı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ortam Temizliği";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Yemek Kalitesi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Hizmet Kalitesi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Fiyat Uygunluğu";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(129, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Ulaşım Kolaylığı";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(214, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Araç Park Durumu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Önerileri Listele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 174);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Sonuçlar Pearson: ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(344, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 17;
+            
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Pearson,
+            this.İndex,
+            this.Kosinüs});
+            this.dataGridView2.Location = new System.Drawing.Point(344, 9);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(296, 150);
+            this.dataGridView2.TabIndex = 19;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(188, 134);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(86, 20);
+            this.textBox8.TabIndex = 20;
+            this.textBox8.Text = "3";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(341, 174);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Sonuçlar Kosinüs: ";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.Restorant});
+            this.dataGridView3.Location = new System.Drawing.Point(344, 190);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(296, 150);
+            this.dataGridView3.TabIndex = 23;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Restoran,
+            this.dataGridViewTextBoxColumn5});
+            this.dataGridView4.Location = new System.Drawing.Point(7, 190);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(296, 150);
+            this.dataGridView4.TabIndex = 24;
+            // 
+            // Restoran
+            // 
+            this.Restoran.HeaderText = "Pearson";
+            this.Restoran.Name = "Restoran";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "Restoran";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // Pearson
+            // 
+            this.Pearson.HeaderText = "Pearson";
+            this.Pearson.Name = "Pearson";
+            // 
+            // İndex
+            // 
+            this.İndex.HeaderText = "İndex";
+            this.İndex.Name = "İndex";
+            // 
+            // Kosinüs
+            // 
+            this.Kosinüs.HeaderText = "Kosinüs";
+            this.Kosinüs.Name = "Kosinüs";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Kosinüs";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // Restorant
+            // 
+            this.Restorant.HeaderText = "Restoran";
+            this.Restorant.Name = "Restorant";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 344);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Restoran;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kosinüs;
+        private System.Windows.Forms.DataGridViewTextBoxColumn İndex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pearson;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Restorant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+    }
+}
+
---
+++ Recomender_System/Recomender_System/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Recomender_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection xlsxbaglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=restoran_oneri.xls; Extended Properties='Excel 12.0 Xml;HDR=YES'");
+        DataTable tablo = new DataTable(); //Verileri direkt datagrid'e çekmek için DataTable kodunu tanımlıyoruz.
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView2.Visible = false;
+            try { 
+            xlsxbaglanti.Open(); //Excel dosyamızığın bağlantısını açıyoruz.
+            tablo.Clear(); //En üstte tanımladığımız Datatable değişkenini temizliyoruz.
+            OleDbDataAdapter da = new OleDbDataAdapter("SELECT * FROM [rating_final$]", xlsxbaglanti);
+            da.Fill(tablo); //Gelen sonuçları datatable'a gönderiyoruz.
+            dataGridView1.DataSource = tablo; //datatable'da ki verileri datagrid'de listeliyoruz.
+            xlsxbaglanti.Close(); //Bağlantıyı kapatıyoruz.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        double  ortalamak = 0, ortalama = 0, toplamk = 0, toplam = 0, kovtop = 0, stsapmak = 0, stsapma = 0,karea = 0,kareb = 0;
+        double cos = 0;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Rows.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView4.Rows.Clear();
+            bool kontrol = true;
+            int[] kullanici = new int[7];
+            try {
+                kullanici[0] = Convert.ToInt32(textBox1.Text);
+                kullanici[1] = Convert.ToInt32(textBox2.Text);
+                kullanici[2] = Convert.ToInt32(textBox3.Text);
+                kullanici[3] = Convert.ToInt32(textBox4.Text);
+                kullanici[4] = Convert.ToInt32(textBox5.Text);
+                kullanici[5] = Convert.ToInt32(textBox6.Text);
+                kullanici[6] = Convert.ToInt32(textBox7.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (kullanici[i] < 1 || kullanici[i] > 10)
+                {
+                    MessageBox.Show("1-10 Arası seçim yapınız..");
+                    kontrol = false;
+                }
+            }
+            if(kontrol == true)
+            { 
+            for(int i = 0; i < 130; i++)
+            {
+                ortalamak = 0; ortalama = 0; toplamk = 0; toplam = 0; kovtop = 0; stsapmak = 0; stsapma = 0; karea = 0; kareb=0;
+                kovtop = 0; cos = 0;
+                //Ortalama için toplam bulma ve cos kısmı
+                for (int j=0; j<7; j++)
+                {
+                    toplamk += kullanici[j];
+                    toplam += Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1].Value);
+                    cos += kullanici[j]* Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1].Value);
+                }
+                ortalama = toplam / 7;
+                ortalamak = toplamk / 7;
+                //stdsapma kısımları,Pearson Kovaryans burası.
+                for (int j = 0; j < 7; j++)
+                {
+                    kovtop += (kullanici[j] - ortalamak) * (Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1].Value) - ortalama);
+                    stsapmak += Math.Pow(kullanici[j] - ortalamak, 2); 
+                    stsapma += Math.Pow(Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1].Value) - ortalama, 2);
+                    karea += Math.Pow(kullanici[j],2);
+                    kareb += Math.Pow(Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1].Value),2);
+                    
+                }
+
+                DataGridViewRow row = (DataGridViewRow)dataGridView2.Rows[0].Clone();
+                if ((Math.Sqrt(stsapmak) * Math.Sqrt(stsapma)) != 0)
+                {
+                    
+                    row.Cells[1].Value = i;
+                    row.Cells[0].Value = kovtop / (Math.Sqrt(stsapmak) * Math.Sqrt(stsapma));
+                    dataGridView2.Rows.Add(row);
+                }
+                else
+                {
+                    row.Cells[1].Value = i;
+                    row.Cells[0].Value = "Sifira Bölme Hatasi..!";
+                    dataGridView2.Rows.Add(row);
+                }
+                if ((Math.Sqrt(karea) * Math.Sqrt(kareb)) != 0)
+                {
+                        dataGridView2.Rows[i].Cells[2].Value = cos / (Math.Sqrt(karea) * Math.Sqrt(kareb));
+                    //row.Cells[2].Value = cos / (Math.Sqrt(karea) * Math.Sqrt(kareb));
+                    //dataGridView2.Rows.Add(row);
+                    }
+                else
+                {
+                        dataGridView2.Rows[i].Cells[2].Value = "Sifira Bölme Hatasi..!";
+                        //row.Cells[2].Value = "Sifira Bölme Hatasi..!";
+                        //dataGridView2.Rows.Add(row);
+                }
+
+                        //richTextBox1.Text += "\n";
+            }
+            dataGridView2.Sort(dataGridView2.Columns["Pearson"], ListSortDirection.Descending);
+            for (int i = 0; i < Convert.ToInt32(textBox8.Text); i++)
+            {
+                DataGridViewRow row2 = (DataGridViewRow)dataGridView4.Rows[0].Clone();
+                row2.Cells[0].Value = dataGridView2.Rows[i].Cells[0].Value;
+                row2.Cells[1].Value = dataGridView1.Rows[Convert.ToInt32(dataGridView2.Rows[i].Cells[1].Value)].Cells[0].Value;
+                dataGridView4.Rows.Add(row2);
+            }
+                //dataGridView4.Sort(dataGridView4.Columns["Restoran"], ListSortDirection.Ascending);
+
+           dataGridView2.Sort(dataGridView2.Columns["Kosinüs"], ListSortDirection.Descending);
+           for (int i = 0; i < Convert.ToInt32(textBox8.Text); i++)
+           {
+                DataGridViewRow row3 = (DataGridViewRow)dataGridView3.Rows[0].Clone();
+                row3.Cells[0].Value = dataGridView2.Rows[i].Cells[2].Value;
+                row3.Cells[1].Value = dataGridView1.Rows[Convert.ToInt32(dataGridView2.Rows[i].Cells[1].Value)].Cells[0].Value;
+                dataGridView3.Rows.Add(row3);
+          }
+
+
+
+         }//if bitiş
+        }
+    }
+}
